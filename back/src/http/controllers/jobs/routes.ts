da46f52bcc291d531ab8c3b823f6a6722bd3c891
*Gustavo <gustavobbacco@gmail.com>
2025-03-25 19:34:00 -0300

    feat: routes to get a job and delete

--- back/src/http/controllers/jobs/routes.ts
+++ back/src/http/controllers/jobs/routes.ts
@@ -2,8 +2,13 @@ import { FastifyInstance } from 'fastify'
 import { fetchJobs } from './fetch-jobs'
 import { create } from './create'
 import { verifyJWT } from '../../../middlewares/verify-jwt'
+import { getJob } from './get'
+import { deleteJob } from './delete'
 
 export async function jobsRoutes(app: FastifyInstance) {
   app.get('/jobs/fetch', fetchJobs)
+  app.get('/jobs/:jobId', { onRequest: [verifyJWT] }, getJob)
+
   app.post('/jobs', { onRequest: [verifyJWT] }, create)
+  app.post('/jobs/:jobId', { onRequest: [verifyJWT] }, deleteJob)
 }
