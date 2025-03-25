f4525ad05651917cc5af804eeb79f44848a625dc
*Gustavo <gustavobbacco@gmail.com>
2025-03-25 20:22:15 -0300

    feat: update a job repository in prisma

--- back/src/repositories/prisma/prisma-jobs-repository.ts
+++ back/src/repositories/prisma/prisma-jobs-repository.ts
@@ -3,6 +3,15 @@ import { JobsRepository } from '../jobs-repository'
 import { prisma } from '../../lib/prisma'
 
 export class PrismaJobsRepository implements JobsRepository {
+  async update(id: string, data: Prisma.JobUpdateInput): Promise<Job> {
+    const job = await prisma.job.update({
+      where: { id },
+      data,
+    })
+
+    return job
+  }
+
   async delete(id: string) {
     await prisma.job.delete({
       where: {
