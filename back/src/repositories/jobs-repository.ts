772c575d0e557ce1f85ffc1dcdaeebec4c870a16
*Gustavo <gustavobbacco@gmail.com>
2025-03-25 19:31:00 -0300

    feat: create findById and delete

--- back/src/repositories/jobs-repository.ts
+++ back/src/repositories/jobs-repository.ts
@@ -2,4 +2,6 @@ import { Job, Prisma } from '@prisma/client'
 
 export interface JobsRepository {
   create(data: Prisma.JobUncheckedCreateInput): Promise<Job>
+  findById(id: string): Promise<Job | null>
+  delete(id: string): Promise<void>
 }
