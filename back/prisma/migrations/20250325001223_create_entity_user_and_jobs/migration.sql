e69f0b4ffbe05e1e69d73140b0a62627ae535742
*Gustavo <gustavobbacco@gmail.com>
2025-03-24 21:24:42 -0300

    feat: create model jobs and JobStatus

---
+++ back/prisma/migrations/20250325001223_create_entity_user_and_jobs/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "JobStatus" AS ENUM ('APPLIED', 'INTERVIEWING', 'OFFERED', 'REJECTED', 'ACCEPTED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "jobs" (
+    "id" TEXT NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "application_status" "JobStatus" NOT NULL DEFAULT 'APPLIED',
+    "description" TEXT NOT NULL,
+    "feedback" TEXT,
+    "link" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "jobs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "jobs" ADD CONSTRAINT "jobs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
