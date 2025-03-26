acd9e156722e63cb16e904360c577b3d11762187
thales0591 <thales0591@gmail.com>
2025-03-26 11:33:36 -0300

    feat: integrate sign-in page with auth context and translate sign-in card to english

--- front/src/pages/auth/sign-in.tsx
+++ front/src/pages/auth/sign-in.tsx
@@ -8,9 +8,9 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { useMutation } from '@tanstack/react-query'
-import { signIn } from '@/api/sign-in'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { authContext } from '../../context/auth-context'
+import { api } from '@/lib/axios'
 
 export interface SignInProps {
   email: string
@@ -18,45 +18,61 @@ export interface SignInProps {
 }
 
 export function SignIn() {
+  const { signIn } = useContext(authContext)
+
   const [email, setEmail] = useState('')
-  const [password, setpassword] = useState('')
+  const [password, setPassword] = useState('')
 
-  const { mutateAsync: authenticate } = useMutation({
-    mutationFn: signIn,
-  })
-
-  async function handleSignIn({ email, password }: SignInProps) {
+  async function handleSignIn(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
     try {
-      const response = await authenticate({ email, password })
-      console.log(response)
+      const { data } = await await api.post('/sign-in', { email, password })
+
+      signIn(data.accessToken)
     } catch (error) {
       console.log(`error: ${error}`)
     }
   }
-  // {...service, [e.target.name] : e.target.value}}
+
   return (
     <main className="flex w-full h-screen justify-center items-center">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-xl font-bold tracking-tight">
-            Entre com sua conta
-          </CardTitle>
-          <CardDescription>Utilize seu email e senha</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="email">E-mail</Label>
-            <Input id="email" placeholder="exemplo@gmail.com" type="email" />
-          </div>
-          <div className="my-5 flex flex-col gap-2">
-            <Label htmlFor="password">Senha</Label>
-            <Input id="password" placeholder="password" type="password" />
-          </div>
-          <Button className="w-full bg-primary" onClick={() => {}}>
-            Entrar
-          </Button>
-        </CardContent>
-      </Card>
+      <form onSubmit={handleSignIn}>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-xl font-bold tracking-tight">
+              Sign-in
+            </CardTitle>
+            <CardDescription>Use your email and password</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="email">E-mail</Label>
+              <Input
+                id="email"
+                placeholder="example@gmail.com"
+                type="email"
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
+            </div>
+            <div className="my-5 flex flex-col gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                placeholder="Password"
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
+            </div>
+            <Button type="submit" className="w-full bg-primary">
+              Log-in
+            </Button>
+          </CardContent>
+        </Card>
+      </form>
     </main>
   )
 }
