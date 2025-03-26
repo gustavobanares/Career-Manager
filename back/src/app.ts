756ea0f86ddb1a02a67d04d8ec1a587307758887
thales0591 <thales0591@gmail.com>
2025-03-26 19:16:26 -0300

    feat: implement global error handler

--- back/src/app.ts
+++ back/src/app.ts
@@ -4,6 +4,7 @@ import { userRoutes } from './http/controllers/users/routes'
 import { env } from './env'
 import { jobsRoutes } from './http/controllers/jobs/routes'
 import fastifyCors from '@fastify/cors'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
@@ -18,3 +19,18 @@ app.register(fastifyCors, {
 
 app.register(userRoutes)
 app.register(jobsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
