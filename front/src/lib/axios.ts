b020e64c275471231508557b5e77023c075e11f1
thales0591 <thales0591@gmail.com>
2025-03-26 11:32:18 -0300

    feat: configure axios interceptors and routes permissions based on auth state

--- front/src/lib/axios.ts
+++ front/src/lib/axios.ts
@@ -1,6 +1,17 @@
+import { accessTokenKey } from '@/context/auth-context'
 import { env } from '@/env'
 import axios from 'axios'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+})
+
+api.interceptors.request.use((config) => {
+  const acessToken = localStorage.getItem(accessTokenKey)
+
+  if (acessToken) {
+    config.headers.Authorization = `Bearer ${acessToken}`
+  }
+
+  return config
 })
