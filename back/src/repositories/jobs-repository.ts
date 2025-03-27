a0213397247a5becd9f9dd126f0c544ffd116d03
*Gustavo <gustavobbacco@gmail.com>
2025-03-27 15:39:01 -0300

    feat: create find many jobs from user in jobs repository

--- back/src/repositories/jobs-repository.ts
+++ back/src/repositories/jobs-repository.ts
@@ -3,6 +3,7 @@ import { Job, Prisma } from '@prisma/client'
 export interface JobsRepository {
   create(data: Prisma.JobUncheckedCreateInput): Promise<Job>
   findById(id: string): Promise<Job | null>
+  findManyByUserId(userId: string): Promise<Job[]>
   delete(id: string): Promise<void>
   update(id: string, data: Prisma.JobUpdateInput): Promise<Job>
 }
