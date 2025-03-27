4534b772a011f2cc06abe4b87e6bd47bffa604c8
thales0591 <thales0591@gmail.com>
2025-03-27 15:47:58 -0300

    test: create user unit tests

---
+++ back/src/use-cases/sign-in.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/users-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { SignInUseCase } from './sign-in'
+import { hash } from 'bcryptjs'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: SignInUseCase
+
+describe('Sign-in use case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new SignInUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to sign-in', async () => {
+    const createdUser = await inMemoryUsersRepository.create({
+      name: 'john doe',
+      email: 'johndoe@gmail.com',
+      password: await hash('1234567', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@gmail.com',
+      password: '1234567',
+    })
+
+    expect(user.name).toEqual('john doe')
+    expect(createdUser.id).toEqual(user.id)
+  })
+})
