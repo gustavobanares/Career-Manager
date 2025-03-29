01ecc1b57539a5141e15efe8bc0ddea1501ba3b8
thales0591 <thales0591@gmail.com>
2025-03-29 14:03:52 -0300

    fix: not going to dashboard componente immediatly after login

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,29 @@
-import { RouterProvider } from 'react-router'
-import { privateRoutes, publicRoutes } from './routes'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { authContext } from './context/auth-context'
+import { Dashboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from './layout/app'
+import { SignUp } from './pages/auth/sign-up'
+import { LandingPage } from './pages/app/landing-page'
 
 export function App() {
   const { isAuthenticated } = useContext(authContext)
 
-  return isAuthenticated ? (
-    <RouterProvider router={privateRoutes} />
-  ) : (
-    <RouterProvider router={publicRoutes} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated ? <AppLayout /> : <LandingPage />}
+        >
+          {isAuthenticated && <Route index element={<Dashboard />} />}
+        </Route>
+
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
